10224a8b7277863511da63c2f31a5bca54f6d553
dxemely <20253ds093@utez.edu.mx>
2026-04-11 13:40:44 -0600

    subiendo carpeta models

--- src/main/java/com/example/integradora/Models/Paciente.java
+++ src/main/java/com/example/integradora/Models/Paciente.java
@@ -1,4 +1,55 @@
 package com.example.integradora.Models;
 
 public class Paciente {
+    private String curp;
+    private String nombre;
+    private int edad;
+    private String telefono;
+    private String alergias;
+    private String estatus;
+
+    public Paciente(String curp, String nombre, int edad, String telefono, String alergias, String estatus) {
+        this.curp = curp;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.telefono = telefono;
+        this.alergias = alergias;
+        this.estatus = estatus;
+    }
+
+    public void setCurp(String curp) {
+        this.curp = curp;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setAlergias(String alergias) {
+        this.alergias = alergias;
+    }
+
+    public void setEstatus(String estatus) {
+        this.estatus = estatus;
+    }
+
+    public String getCurp() { return curp; }
+    public String getNombre() { return nombre; }
+    public int getEdad() { return edad; }
+    public String getTelefono() { return telefono; }
+    public String getAlergias() { return alergias; }
+    public String getEstatus() { return estatus; }
+
+
+    public String toCsv() {
+        return curp + "," + nombre + "," + edad + "," + telefono + "," + alergias + "," + estatus;
+    }
 }
