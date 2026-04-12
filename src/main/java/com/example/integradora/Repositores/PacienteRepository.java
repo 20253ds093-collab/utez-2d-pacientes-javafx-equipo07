c4ce66b4327c282324a32395840ce2a6bf18b6d1
dxemely <20253ds093@utez.edu.mx>
2026-04-12 17:22:18 -0600

    Arrelgando orden paciente y cambiando el repository

--- src/main/java/com/example/integradora/Repositores/PacienteRepository.java
+++ src/main/java/com/example/integradora/Repositores/PacienteRepository.java
@@ -2,29 +2,33 @@ package com.example.integradora.Repositores;
 
 import com.example.integradora.Models.Paciente;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PacienteRepository {
-    private final Path filePath = Paths.get("data", "pacientes.csv");
+    private final Path path = Paths.get("data", "pacientes.csv");
 
     public List<Paciente> findAll() throws IOException {
-        if (Files.notExists(filePath)) Files.createFile(filePath);
-        List<String> lines = Files.readAllLines(filePath, StandardCharsets.UTF_8);
+        if (!Files.exists(path)) {
+            Files.createDirectories(path.getParent());
+            Files.createFile(path);
+        }
+        List<String> lines = Files.readAllLines(path);
         List<Paciente> lista = new ArrayList<>();
-        for (String line : lines) {
-            if (line.isBlank()) continue;
-            String[] p = line.split(",");
-            lista.add(new Paciente(p[0], p[1], Integer.parseInt(p[2]), p[3], p[4], p[5]));
+        for (String l : lines) {
+            if (l.isBlank()) continue;
+            String[] d = l.split(",");
+            lista.add(new Paciente(d[0], d[1], Integer.parseInt(d[2]), d[3], d[4], d[5]));
         }
         return lista;
     }
 
-    public void saveAll(List<Paciente> pacientes) throws IOException {
+    public void saveAll(List<Paciente> lista) throws IOException {
         List<String> lines = new ArrayList<>();
-        for (Paciente p : pacientes) lines.add(p.toCsv());
-        Files.write(filePath, lines, StandardOpenOption.TRUNCATE_EXISTING);
+        for (Paciente p : lista) {
+            lines.add(p.toCsv());
+        }
+        Files.write(path, lines);
     }
 }
